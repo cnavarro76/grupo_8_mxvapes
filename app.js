f2770981f15eaece15176f2b9ce2beadc0623f7f
rocioazucena8576 <47404584+rocioazucena8576@users.noreply.github.com>
2021-11-07 23:43:00 -0600

    inicio de index

--- app.js
+++ app.js
@@ -8,26 +8,26 @@ const port = 3030;
 
 app.use(express.static('public'));
 
-app.listen (port, () => console.log(`Servidor corriendo en el puerto ${port}`));
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname + '/views/index.html'));
 });
 
-app.get('/productDetail', function(req, res){
-    res.sendFile(path.resolve(__dirname,'./views/productDetail.html'));
+app.get('/productDetail', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
 })
 
-app.get('/productCart', function(req, res){
-    res.sendFile(path.resolve(__dirname,'./views/productCart.html'));
+app.get('/productCart', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
 })
 
-app.get('/register', function(req, res){
-    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+app.get('/register', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 })
 
-app.get('/login', function(req, res){
-    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+app.get('/login', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 })
 
 app.get('*', (req, res) => {
