f561beff5292c86c545dd1fcc13287788ec5c24d
cnavarro76 <ncarlos47@hotmail.com>
2021-11-11 22:35:55 -0600

    nuevos archivos

--- app.js
+++ app.js
@@ -14,21 +14,23 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname + '/views/index.html'));
 });
 
-app.get('/productDetail', function (req, res) {
-    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+app.get('/login', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 })
 
 app.get('/productCart', function (req, res) {
     res.sendFile(path.resolve(__dirname, './views/productCart.html'));
 })
 
+app.get('/productDetail', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+})
+
 app.get('/register', function (req, res) {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 })
 
-app.get('/login', function (req, res) {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
-})
+
 
 app.get('*', (req, res) => {
     res.status(404).send('404 not found. <br> ¡Houston, we have a problem!');
