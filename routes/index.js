5e938485a6935af34071e0bd7b47fe92c0a2dc5e
rocioazucena8576 <47404584+rocioazucena8576@users.noreply.github.com>
2022-03-09 22:48:58 -0600

    imagenes index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
+
+router.get('/productDetail', function (req, res, next) {
+  res.render('detalleProducto', {
+    title: 'Express'
+  });
+});
+
 
 module.exports = router;
